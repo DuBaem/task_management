bf420048f68bacc057eeaa5f944b5fcc7351b1a6
DuBaem <dubem.akukwe@gmail.com>
2025-04-29 21:08:05 -0500

    Urls now lead to signup and login pages. No css yet

--- stage/views.py
+++ stage/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
+
 # Create your views here.
-# def home(request):
-#     return render(request, 'stage/index.html')
-
 def home(request):
-     return(HttpResponse('<h1>Task hOme</h1>'))
+    return render(request, 'stage/index.html')
 
-def about(request):
-    return(HttpResponse('<h1>Task Sign</h1>'))
+def signup(request):
+    return render(request, 'stage/signup.html')
 
