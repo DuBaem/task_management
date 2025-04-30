db9d09098b1c1a1f0f946818692729d2a6ac88dd
DuBaem <dubem.akukwe@gmail.com>
2025-04-30 12:41:44 -0500

    users app made and added to settings

--- task_management/settings.py
+++ task_management/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'stage.apps.StageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
